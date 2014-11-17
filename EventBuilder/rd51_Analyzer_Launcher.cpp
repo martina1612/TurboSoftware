7307bb1b95d5febe83ed12475285a3f44eac5bb9
Ram Krishna Sharma <ramkrishna.sharma71@gmail.com>
2014-11-17 22:09:45 +0100

    New_Turbo_Soft

--- EventBuilder/rd51_Analyzer_Launcher.cpp
+++
@@ -1,6 +0,0 @@
-void rd51_Analyzer_Launcher(TString InputRecoFile, TString InputTrackerFile, TString OutputAnalyzerFile)
-{
-  gROOT->ProcessLine(".x Load_Analyzer.cpp");
-  rd51_Analyzer(InputRecoFile,InputTrackerFile,OutputAnalyzerFile);
-}
-
--- EventBuilder/shrd51_ClassMaker_RecoTracker.sh
+++
@@ -1,4 +0,0 @@
-source rd51_SetEnvironment.sh
-rm -v -f Class_Reco*.*
-rm -v -f Class_Tracker*.*
-root -b -l -q 'ClassMaker_RecoTracker.cpp("'$1'","'$2'")'
